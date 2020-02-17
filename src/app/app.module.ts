bc3510f2350a123d41956c116efc0c6fc449527b
Presutti Pasquale <p.presutti@reply.it>
2020-02-17 14:13:13 +0100

    Aggiunta Config FireBase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from '@angular/fire'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AlbumComponent } from './components/album/album.component';
+import { AngularFirestore } from '@angular/fire/firestore';
+
+const config = {
+   apiKey: "AIzaSyBWSShZBgrgA3UVdmNt9T_YkjjZvMHTc2A",
+   authDomain: "mancolista-db.firebaseapp.com",
+   databaseURL: "https://mancolista-db.firebaseio.com",
+   projectId: "mancolista-db",
+   storageBucket: "mancolista-db.appspot.com",
+   messagingSenderId: "846235428676",
+};
 
 @NgModule({
    declarations: [
@@ -12,9 +22,10 @@ import { AlbumComponent } from './components/album/album.component';
    ],
    imports: [
       BrowserModule,
-      AppRoutingModule
+      AppRoutingModule,
+      AngularFireModule.initializeApp(config),
    ],
-   providers: [],
+   providers: [AngularFirestore],
    bootstrap: [
       AppComponent
    ]
